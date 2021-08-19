e0de36aed6dc4b175544dfd5ee9fda351bad5651
KawaiiiZoe <84874606+KawaiiiZoe@users.noreply.github.com>
2021-08-19 20:08:59 +0200

    Remove icon method, and fix the nonnull error.

--- src/main/java/systems/tat/ramta/client/models/lang/Language.java
+++ src/main/java/systems/tat/ramta/client/models/lang/Language.java
@@ -1,19 +1,17 @@
 package systems.tat.ramta.client.models.lang;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import systems.tat.ramta.client.service.settings.handler.JSONHandler;
-import systems.tat.ramta.client.utils.ResourcesUtils;
 
 import java.io.File;
-import java.util.Objects;
 
 public class Language {
 
-    private final File RESOURCE_LOCATION = new File(ResourcesUtils.getPath("frontend/displayed"));
+    private final Logger logger = LoggerFactory.getLogger(Language.class);
 
     private final String name;
     private final String unicode;
-    private final String flagIconPath;
-    private final File icon;
     private final File languageFile;
 
     public Language(File languageFile) {
@@ -21,13 +19,6 @@ public class Language {
         String[] object = languageFile.getName().split("\\.");
         this.name = object[0];
         this.unicode = read("unicode");
-        File file = null;
-        for(File files : Objects.requireNonNull(RESOURCE_LOCATION.listFiles())) {
-            file = files;
-        }
-        assert file != null;
-        this.flagIconPath = RESOURCE_LOCATION.getPath() + "/" + file.getName() + "/" + read("flagIcon");
-        this.icon = new File(flagIconPath);
     }
 
     public String getName() {
@@ -42,16 +33,8 @@ public class Language {
         return languageFile;
     }
 
-    public String getFlagIconPath() {
-        return flagIconPath;
-    }
-
-    public String getFlagURL() {
-        return "file:///" + flagIconPath.replaceAll(" ", "%20");
-    }
-
-    public File getIcon() {
-        return icon;
+    public Logger getLogger() {
+        return logger;
     }
 
     private String read(String key) {
